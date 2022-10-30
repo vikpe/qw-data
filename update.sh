36ab16a864d591f8d1c86a50f044b1266ebf0dae
Viktor Persson <viktor.persson@arcsin.se>
2022-10-30 20:22:25 +0100

    Add ezquake.

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 mkdir -p github
+curl https://api.github.com/repos/qw-group/ezquake/releases -o github/ezquake_releases.json
+curl https://api.github.com/repos/qw-group/ezquake/releases/latest -o github/ezquake_latest_release.json
 curl https://api.github.com/repos/qw-group/mvdsv/releases/latest -o github/mvdsv_latest_release.json
 curl https://api.github.com/repos/qw-group/ktx/releases/latest -o github/ktx_latest_release.json
 curl https://api.github.com/repos/qw-group/qtv/releases/latest -o github/qtv_latest_release.json
