683779aadc2feb58e755afb373c396333d7b6cb0
Viktor Persson <viktor.persson@arcsin.se>
2022-10-30 19:40:45 +0100

    Add update script.

---
+++ update.sh
@@ -0,0 +1,5 @@
+mkdir -p github
+curl https://api.github.com/repos/qw-group/mvdsv/releases/latest -o github/mvdsv_latest_release.json
+curl https://api.github.com/repos/qw-group/ktx/releases/latest -o github/ktx_latest_release.json
+curl https://api.github.com/repos/qw-group/qtv/releases/latest -o github/qtv_latest_release.json
+curl https://api.github.com/repos/qw-group/qwfwd/releases/latest -o github/qwfwd_latest_release.json
