c35afc27966c1267c48befc7cbed0c9415697525
Viktor Persson <viktor.persson@arcsin.se>
2022-12-04 20:42:56 +0100

    Add script to write server list of european mvdsv servers.

---
+++ update_from_hubapi.sh
@@ -0,0 +1,5 @@
+apt install jq -y
+
+mkdir -p hubapi
+
+curl https://hubapi.quakeworld.nu/v2/servers | jq --raw-output '.[] | select(.geo.region == "Europe" and (.version | startswith("MVDSV"))) | .settings.hostname_parsed' | sort > hubapi/servers_mvdsv_europe.txt
