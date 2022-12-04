28c48858ff618ea23094dedaf83b774b9836f118
Viktor Persson <viktor.persson@arcsin.se>
2022-12-04 20:48:10 +0100

    Remove obsolete installation command.

--- update_from_hubapi.sh
+++ update_from_hubapi.sh
@@ -1,5 +1,3 @@
-sudo apt install jq -y
-
 mkdir -p hubapi
 
 curl https://hubapi.quakeworld.nu/v2/servers | jq --raw-output '.[] | select(.geo.region == "Europe" and (.version | startswith("MVDSV"))) | .settings.hostname_parsed' | sort > hubapi/servers_mvdsv_europe.txt
