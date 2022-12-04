f86d515a16e77e64f61cc8b66c3e69a5bc7778e9
Viktor Persson <viktor.persson@arcsin.se>
2022-12-04 20:55:18 +0100

    Print count of servers written.

--- update_from_hubapi.sh
+++ update_from_hubapi.sh
@@ -1,3 +1,4 @@
 mkdir -p hubapi
 
 curl https://hubapi.quakeworld.nu/v2/servers | jq --raw-output '.[] | select(.geo.region == "Europe" and (.version | startswith("MVDSV"))) | .settings.hostname_parsed' | sort > hubapi/servers_mvdsv_europe.txt
+wc -l hubapi/servers_mvdsv_europe.txt
