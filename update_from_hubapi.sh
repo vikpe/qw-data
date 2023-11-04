a68dfbee96b410edce64a9d67e6bbcdd238faeb3
Viktor Persson <vikpe@users.noreply.github.com>
2023-11-04 11:58:57 +0100

    Add server list for north america.

--- update_from_hubapi.sh
+++ update_from_hubapi.sh
@@ -2,3 +2,6 @@ mkdir -p hubapi
 
 curl https://hubapi.quakeworld.nu/v2/servers | jq --raw-output '.[] | select(.geo.region == "Europe" and (.version | startswith("MVDSV"))) | .settings.hostname_parsed' | sort > hubapi/servers_mvdsv_europe.txt
 wc -l hubapi/servers_mvdsv_europe.txt
+
+curl https://hubapi.quakeworld.nu/v2/servers | jq --raw-output '.[] | select(.geo.region == "North America" and (.version | startswith("MVDSV"))) | .settings.hostname_parsed' | sort > hubapi/servers_mvdsv_north_america.txt
+wc -l hubapi/servers_mvdsv_north_america.txt
