15a5813ba696d2cc2f636c2970a0e2ff4ead1c03
Viktor Persson <vikpe@users.noreply.github.com>
2026-02-14 16:59:39 +0100

    Fix jq command syntax in update_from_hubapi.sh

--- update_from_hubapi.sh
+++ update_from_hubapi.sh
@@ -2,11 +2,11 @@ mkdir -p hubapi
 
 curl https://hubapi.quakeworld.nu/v2/servers > servers.json
 
-jq servers.json --raw-output '.[] | select(.geo.region == "Europe" and (.version | startswith("MVDSV"))) | .settings.hostname_parsed' | sort > hubapi/servers_mvdsv_europe.txt
+jq --raw-output '.[] | select(.geo.region == "Europe" and (.version | startswith("MVDSV"))) | .settings.hostname_parsed' servers.json | sort > hubapi/servers_mvdsv_europe.txt
 wc -l hubapi/servers_mvdsv_europe.txt
 
-jq servers.json --raw-output '.[] | select(.geo.region == "North America" and (.version | startswith("MVDSV"))) | .settings.hostname_parsed' | sort > hubapi/servers_mvdsv_north_america.txt
+jq --raw-output '.[] | select(.geo.region == "North America" and (.version | startswith("MVDSV"))) | .settings.hostname_parsed' servers.json | sort > hubapi/servers_mvdsv_north_america.txt
 wc -l hubapi/servers_mvdsv_north_america.txt
 
-jq servers.json --raw-output '.[] | select(.geo.region == "South America" and (.version | startswith("MVDSV"))) | .settings.hostname_parsed' | sort > hubapi/servers_mvdsv_south_america.txt
+jq --raw-output '.[] | select(.geo.region == "South America" and (.version | startswith("MVDSV"))) | .settings.hostname_parsed' servers.json | sort > hubapi/servers_mvdsv_south_america.txt
 wc -l hubapi/servers_mvdsv_south_america.txt
