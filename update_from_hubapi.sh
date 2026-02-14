939e35cedbb32b7d8f49c3a9af39cc0a77e20358
Viktor Persson <vikpe@users.noreply.github.com>
2026-02-14 16:55:02 +0100

    Refactor server data fetching and add South America support
    Updated script to fetch server data into a JSON file before processing. Added support for South America region.

--- update_from_hubapi.sh
+++ update_from_hubapi.sh
@@ -1,7 +1,12 @@
 mkdir -p hubapi
 
-curl https://hubapi.quakeworld.nu/v2/servers | jq --raw-output '.[] | select(.geo.region == "Europe" and (.version | startswith("MVDSV"))) | .settings.hostname_parsed' | sort > hubapi/servers_mvdsv_europe.txt
+curl https://hubapi.quakeworld.nu/v2/servers > servers.json
+
+jq servers.json --raw-output '.[] | select(.geo.region == "Europe" and (.version | startswith("MVDSV"))) | .settings.hostname_parsed' | sort > hubapi/servers_mvdsv_europe.txt
 wc -l hubapi/servers_mvdsv_europe.txt
 
-curl https://hubapi.quakeworld.nu/v2/servers | jq --raw-output '.[] | select(.geo.region == "North America" and (.version | startswith("MVDSV"))) | .settings.hostname_parsed' | sort > hubapi/servers_mvdsv_north_america.txt
+jq servers.json --raw-output '.[] | select(.geo.region == "North America" and (.version | startswith("MVDSV"))) | .settings.hostname_parsed' | sort > hubapi/servers_mvdsv_north_america.txt
 wc -l hubapi/servers_mvdsv_north_america.txt
+
+jq servers.json --raw-output '.[] | select(.geo.region == "South America" and (.version | startswith("MVDSV"))) | .settings.hostname_parsed' | sort > hubapi/servers_mvdsv_south_america.txt
+wc -l hubapi/servers_mvdsv_south_america.txt
